5b6a5f97f9a79f9ae1b66d34590c089b9b350079
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-12-14 22:59:45 -0800

    Syntax cleanup and ES6

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-const request = require("request");
-const cheerio = require("cheerio");
+const request = require('request');
+const cheerio = require('cheerio');
 
 // Require all models
-const db = require("./models");
+const db = require('./models');
 
 const PORT = 3050;
 
@@ -19,31 +19,31 @@ const app = express();
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongo_scraper", {
+mongoose.connect('mongodb://localhost/mongo_scraper', {
   useMongoClient: true
 });
 
 // Routes
 
 // A GET route for scraping the echojs website
-app.get("/scrape", function(req, res) {
-  request("http://www.foodnetwork.com/",(error, response, html) => {
+app.get('/scrape', (req, res) => {
+  request('http://www.foodnetwork.com/',(error, response, html) => {
 
     const $ = cheerio.load(html);
 
     // An empty array to save the data that we'll scrape
     let results = [];
 
-    $("div.m-MediaBlock--gallery").each(function(i, element) {
+    $('div.m-MediaBlock--gallery').each((i, element) => {
 
-      const link = $(element).children().find("a").attr("href");
-      const title = $(element).children().find("span.m-MediaBlock__a-HeadlineText").text();
-      const description = $(element).children().find("div.m-MediaBlock__a-Description").text();
+      const link = $(element).children().find('a').attr('href');
+      const title = $(element).children().find('span.m-MediaBlock__a-HeadlineText').text();
+      const description = $(element).children().find('div.m-MediaBlock__a-Description').text();
 
       if (link && title && description) {
         results.push({
@@ -55,11 +55,11 @@ app.get("/scrape", function(req, res) {
     });
     db.Article
       .create(results)
-      .then(function(dbArticle) {
+      .then((dbArticle) => {
         // If we were able to successfully scrape and save an Article, send a message to the client
-        res.send("Scrape Complete");
+        res.send('Scrape Complete');
       })
-      .catch(function(err) {
+      .catch((err) => {
         // If an error occurred, send it to the client
         res.json(err);
       });
@@ -67,54 +67,54 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get('/articles', (req, res) => {
   db.Article
     .find({})
-    .then(function(articles) {
+    .then((articles) => {
       // If any Books are found, send them to the client
       res.json(articles);
     })
-    .catch(function(err) {
+    .catch((err) => {
       // If an error occurs, send it back to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get('/articles/:id', (req, res) => {
   db.Article
     .findById(req.params.id)
-    .populate("note")
-    .then(function(articles) {
+    .populate('note')
+    .then((articles) => {
       // If any Books are found, send them to the client
       res.json(articles);
       console.log(articles);
     })
-    .catch(function(err) {
+    .catch((err) => {
       // If an error occurs, send it back to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post('/articles/:id', (req, res) => {
   db.Note
     .create(req.body)
-    .then(function(dbNote) {
+    .then((dbNote) => {
       // If we were able to successfully scrape and save an Article, send a message to the client
       //res.send("Scrape Complete");
       return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbNote._id } }, { new: true });
     })
-    .then(function(data) {
+    .then((data) => {
       res.json(data);
     })
-    .catch(function(err) {
+    .catch((err) => {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
